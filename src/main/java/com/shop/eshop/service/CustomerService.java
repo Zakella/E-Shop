d8ff1d22d4eb453e7b287180fcce324ca9c12e7b
Zakella <zapolski.slava@gmail.com>
2022-12-23 20:53:39 +0200

    added actuator

--- src/main/java/com/shop/eshop/service/CustomerService.java
+++ src/main/java/com/shop/eshop/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.shop.eshop.service;
 import com.shop.eshop.exception.NotFoundException;
 import com.shop.eshop.model.Customer;
 import com.shop.eshop.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 @Service
 public class CustomerService {
 
+    private static final Logger Logger = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
 
@@ -28,8 +31,12 @@ public class CustomerService {
     public Customer getCustomer(Long id){
         return customerRepository.
                 findById(id)
-                .orElseThrow(()->new NotFoundException("Customer width id " + id + " not found"));
-
+                .orElseThrow(()->
+                {
+                    NotFoundException notFoundException = new NotFoundException("Customer width id " + id + " not found");
+                    Logger.error("Error in customer width id {}", id);
+                    return notFoundException;
+                });
     }
 
     public Customer save(Customer customer) {
