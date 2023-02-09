6bd08534a684fc40028b46fdd24f1884a024ca65
Zakella <zapolski.slava@gmail.com>
2023-02-09 14:40:17 +0200

    add basic auth config

--- src/main/java/com/shop/eshop/security/basic_auth/BasicAuthConfig.java
+++ src/main/java/com/shop/eshop/security/basic_auth/BasicAuthConfig.java
@@ -1,4 +1,41 @@
 package com.shop.eshop.security.basic_auth;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class BasicAuthConfig {
+
+//    public SecurityConfig(JpaUserDetailsService myUserDetailsService) {
+//        this.myUserDetailsService = myUserDetailsService;
+//    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/","index","/css/*","/js/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+//                .userDetailsService(myUserDetailsService)
+//                .headers(headers -> headers.frameOptions().sameOrigin())
+                .httpBasic(withDefaults())
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/cabinet", true)
+                .and()
+                .build();
+    }
+
+
 }
